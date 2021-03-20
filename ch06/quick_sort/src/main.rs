22eff581043e03a80c4218459aa8904753e7ad4c
jameskk <jamesmarva@users.noreply.github.com>
2021-03-21 00:16:14 +0800

    add ch06

---
+++ ch06/quick_sort/src/main.rs
@@ -0,0 +1,24 @@
+fn main() {
+    let a = [1, 3, 4, 3, 2, 11];
+    for item in &a {
+        println!("{}", item);
+    }
+
+}
+/// double point 
+fn quick_sort(arr: &mut [i32]) {
+
+}
+
+fn qck_sort(arr: &mut [i32], start: i32, end: i32) {
+
+}
+fn quick_sort_core(arr: &mut [i32]) {
+    let len = arr.len();
+    let val = arr[0];
+    let idx = 0;
+    let mut l = 0;
+    let mut r = len;
+    while true {
+    }
+}
