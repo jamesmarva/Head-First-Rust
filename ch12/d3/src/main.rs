0b408ac737e0c33597ab1e50cd2ae031c7ae8184
jameskk <jamesmarva@users.noreply.github.com>
2021-04-28 22:49:43 +0800

    update ch12

--- ch12/d3/src/main.rs
+++ ch12/d3/src/main.rs
@@ -1,3 +1,14 @@
 fn main() {
-    println!("Hello, world!");
+    let mut v = vec![];
+
+    foo(&mut v);
+
+    println!("{:?}", v);
+    println!("{:?}", v);
+    }
+
+fn foo (v: &mut Vec<i32>) {
+    v.push(4)
 }
+
+
--- ch12/d4/src/main.rs
+++ ch12/d4/src/main.rs
@@ -1,3 +1,32 @@
+/// mut 如果绑定的是变量，就表示变量能被重新帮
+/// mut 如果修饰的是 引用，也就是“借用指针”，那么代表着，这个指针指向的对象可以被修改。
 fn main() {
-    println!("Hello, world!");
+    let mut var = 0_i32;
+    {
+        let p1 = &mut var;
+        *p1 = 1;
+        println!("var {}", var);
+    }
+
+    {
+        let temp  = 2_i32;
+        let mut p2 = &var;
+        p2 = &temp;
+        println!("p2 {}", p2);
+    }
+    println!("var {}", var);
+
+    {
+        let mut temp = 3_i32;
+        let mut p3 = &mut var;
+        *p3 = 4_i32;
+        println!("p3 {}", p3);
+
+        p3 = &mut temp;
+        println!("p3 {}", p3);
+
+    }
+    println!("var {}", var);
 }
+
+
