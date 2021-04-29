98f230998ea0d6ad16feb4acde9d7b70ed9ba525
wanghanwei <jamesmarva@users.noreply.github.com>
2021-04-29 12:41:24 +0800

    update ch12

--- ch12/s4_d1/src/main.rs
+++ ch12/s4_d1/src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+/// 借用指针类型的都有一个生命周期泛型参数。
+struct T {
+    member: i32,
 }
+
+fn test<'a>(arg: &'a T) -> &'a i32 
+{
+    &arg.member
+}
+
+fn main() {
+    let t = T{member: 0};
+    let x = test(&t);
+    println!("{:?}", x);
+}
+
--- ch12/s4_d2/src/main.rs
+++ ch12/s4_d2/src/main.rs
@@ -1,3 +1,22 @@
 fn main() {
-    println!("Hello, world!");
+    let t = T {member: 0};
+    let x = test(&t);
+    println!("{}", x);
+
+    let x1 = t1(&t);
+    println!("x1: {}", x1);
+}
+
+struct T {
+    member: i32,
+}
+
+fn test<'a>(arg: &'a T) -> &'a i32 {
+    &arg.member
+}
+
+fn t1<'a, 'b>(arg: &'a T) -> &'a i32 
+    where 'a: 'b
+{
+    &arg.member
 }
