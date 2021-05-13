5e301400691147143c42221e0c0237ae9dda8286
jameskk <jamesmarva@users.noreply.github.com>
2021-05-14 00:50:56 +0800

    add ch21 s5

--- ch21/s5/d2/src/main.rs
+++ ch21/s5/d2/src/main.rs
@@ -1,3 +1,17 @@
+use std::iter::Iterator;
+use std::fmt::Debug;
+use std::fmt::{Display, Formatter, Result};
+
+fn use_iter<ITEM, ITER>(mut iter: ITER) 
+where ITER: Iterator<Item=ITEM>,
+    ITEM: Display
+{
+    while let Some(i) = iter.next() {
+        println!("{}", i)
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let v = vec![1, 2, 3, 4];
+    use_iter(v.iter())
 }
--- ch21/s5/d3/src/main.rs
+++ ch21/s5/d3/src/main.rs
@@ -1,3 +1,27 @@
+use std::iter::Iterator;
+use std::fmt::Display;
+
+fn all<ITER>(mut iter: ITER) 
+where ITER: Iterator,
+    ITER::Item: Display
+{
+    while let Some(i) = iter.next() {
+        println!("{}", i);
+    }
+}
+
+fn all_1<ITER, ITEM>(mut iter: ITER) 
+where ITER: Iterator<Item=ITEM>,
+    ITEM: Display
+{
+    while let Some(i) = iter.next() {
+        println!("{}", i);
+    }
+}
+
+
 fn main() {
-    println!("Hello, world!");
+    let v = vec![1, 2, 3, 4, 5];
+    all(v.iter());
+
 }
