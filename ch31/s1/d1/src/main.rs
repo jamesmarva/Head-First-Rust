377540a0a54531326830d50bf6dd6723fc103bad
jameskk <jamesmarva@users.noreply.github.com>
2021-05-25 02:52:41 +0800

    Update main.rs

--- ch31/s1/d1/src/main.rs
+++ ch31/s1/d1/src/main.rs
@@ -4,16 +4,18 @@ use std::sync::mpsc::channel;
 
 fn main() {
 
+    println!("fffffffffffffffffffff");
     let n_workers = 4;
     let n_jobs = 8;
-
     let pool = ThreadPool::new(n_workers);
-    let (s, r) = channel();
+
+    let (tx, rx) = channel();
     for _ in 0..n_jobs {
-        let s_clone = s.clone();
-        pool.execute(move || {
-            s_clone.send(1).expect("channel will be there waiting for the pool");
+        let tx = tx.clone();
+        pool.execute(move|| {
+            tx.send(1).expect("channel will be there waiting for the pool");
         });
-        assert_eq!(r.iter().take(n_jobs).fold(0, |a, b| a + b), 8);
     }
+
+    assert_eq!(rx.iter().take(n_jobs).fold(0, |a, b| a + b), 8);
 }
