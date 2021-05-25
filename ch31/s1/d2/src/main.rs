8e1c147c61861dac58ddcd377a5280f1e591a587
wanghanwei <jamesmarva@users.noreply.github.com>
2021-05-25 22:12:27 +0800

    add ch31

--- ch31/s1/d2/src/main.rs
+++ ch31/s1/d2/src/main.rs
@@ -1,3 +1,23 @@
+extern crate threadpool;
+
+use threadpool::ThreadPool;
+use std::sync::mpsc::channel;
+
 fn main() {
-    println!("Hello, world!");
+
+    let n_workers = 4;
+    let n_jobs = 8;
+
+    let pool = ThreadPool::new(n_workers);
+
+    let (sender, receiver) = channel();
+
+    for _ in 0..n_jobs {
+        let new_sender = sender.clone();
+        pool.execute(move ||{
+            new_sender.send(1).expect("channel wil be there waitging for the pool.");
+        });
+    }
+
+    println!("{}", receiver.iter().take(n_jobs).fold(0, |a, b| a + b));
 }
