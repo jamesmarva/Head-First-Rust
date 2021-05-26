e6f24a09eca016cca16a12089d0caec6397dbc5c
jameskk <jamesmarva@users.noreply.github.com>
2021-05-27 03:37:47 +0800

    add ch31

--- ch31/s1/d5/src/main.rs
+++ ch31/s1/d5/src/main.rs
@@ -1,3 +1,48 @@
+extern crate pond;
+use pond as pd;
+
 fn main() {
-    println!("Hello, world!");
+    // test_new();
+    test_new_threads();
 }
+
+
+fn test_new() {
+    let mut v = vec![1, 2, 3, 4, 5, 6];
+    let mut pool = pd::Pool::new();
+
+    pool.scoped( |sco| {
+        let local_sco = sco.with_state(|| 11);
+
+        for i in &mut v {
+            local_sco.execute( 
+                move |state| {
+                    *i += *state;
+                    println!("*i: {}; *state: {}", *i, *state);
+                }
+            );
+        }
+    });
+}
+
+
+fn test_new_threads() {
+    let mut v = vec![1, 2, 3, 4, 5];
+    let mut pool = pd::Pool::new_threads(5, 100);
+    pool.scoped(|s| {
+            let local_scope = s.with_state(|| 111);
+            for i in &mut v {
+                local_scope.execute( 
+                    move |state| {
+                        *i += *state;
+                        println!("i {}; state {}", i, state);
+                    }
+                );
+            }
+        }
+    );
+}
+
+
+
+
--- ch31/s1/d6/src/main.rs
+++ ch31/s1/d6/src/main.rs
@@ -1,3 +1,15 @@
+
+
+use std::collections::VecDeque;
+use std::sync::{Arc, Mutex, Condvar};
+use std::thread::JoinHandler;
+use std::marker::PhantomData;
+use std::any::Any;
+
+extren crate num_cpus;
+
+
 fn main() {
-    println!("Hello, world!");
 }
+
+
